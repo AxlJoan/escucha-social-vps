901d03ea283f4c5adae769e4bf7408eb26a65dbb
TheDaffyFife1 <castillotorresoscar@gmail.com>
2024-04-03 09:46:48 -0500

    beta

---
+++ palabras_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PalabrasAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'palabras_admin'
---
+++ palabras_admin/models.py
@@ -0,0 +1,103 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+from .roles import ADMIN_ROLE, VIEWER 
+from django.contrib.auth.models import User
+
+class Extraccion(models.Model):
+    chat_row_id = models.IntegerField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    received_timestamp = models.DateTimeField(blank=True, null=True)
+    text_data = models.TextField(blank=True, null=True)
+    from_me = models.IntegerField(blank=True, null=True)
+    number = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    verified_name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    cliente = models.CharField(max_length=255, blank=True, null=True)
+    estado = models.CharField(max_length=255, blank=True, null=True)
+    municipio = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'extraccion'
+
+
+class Extraccion2(models.Model):
+    chat_row_id = models.IntegerField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    received_timestamp = models.DateTimeField(blank=True, null=True)
+    text_data = models.TextField(blank=True, null=True)
+    from_me = models.IntegerField(blank=True, null=True)
+    number = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    verified_name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    cliente = models.CharField(max_length=255, blank=True, null=True)
+    estado = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'extraccion2'
+
+
+class Extraccion3(models.Model):
+    chat_row_id = models.IntegerField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    received_timestamp = models.DateTimeField(blank=True, null=True)
+    text_data = models.TextField(blank=True, null=True)
+    from_me = models.IntegerField(blank=True, null=True)
+    number = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    verified_name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    cliente = models.CharField(max_length=255, blank=True, null=True)
+    estado = models.CharField(max_length=255, blank=True, null=True)
+    municipio = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'extraccion3'
+
+
+class Extraccion4(models.Model):
+    chat_row_id = models.IntegerField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    received_timestamp = models.DateTimeField(blank=True, null=True)
+    text_data = models.TextField(blank=True, null=True)
+    from_me = models.IntegerField(blank=True, null=True)
+    number = models.CharField(max_length=255, blank=True, null=True, db_index=True)  # Individual field index
+    number2 = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    verified_name = models.CharField(max_length=255, blank=True, null=True)
+    server = models.CharField(max_length=255, blank=True, null=True)
+    device = models.CharField(max_length=255, blank=True, null=True)
+    group_name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    cliente = models.CharField(max_length=255, blank=True, null=True, db_index=True)  # Individual field index
+    estado = models.CharField(max_length=255, blank=True, null=True, db_index=True)  # Individual field index
+    municipio = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'extraccion4'
+        indexes = [
+            models.Index(fields=['number', 'cliente'], name='number_cliente_idx'),  # Composite index
+            models.Index(fields=['estado', 'municipio'], name='estado_municipio_idx'),  # Composite index
+        ]
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    role = models.CharField(max_length=50, choices=[
+        (ADMIN_ROLE, 'Admin'),
+        (VIEWER, 'visualizador'),
+    ])
+    def __str__(self):
+        return self.user.username
+    
